71fa6ee28068e2266ab53dbbdad74f3d23a6c784
scherbovich1 <andrewscherbovich@gmail.com>
2020-11-13 16:26:39 +0300

    Initial commit

---
+++ app/src/main/java/andrew/developer/demoapplication/data/entities/AlbumsItem.kt
@@ -0,0 +1,12 @@
+package andrew.developer.demoapplication.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class AlbumsItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("userId")
+    val userId: Int
+)
---
+++ app/src/main/java/andrew/developer/demoapplication/data/network/AlbumsAPI.kt
@@ -0,0 +1,21 @@
+package andrew.developer.demoapplication.data.network
+
+import andrew.developer.demoapplication.data.entities.Albums
+import andrew.developer.demoapplication.data.entities.AlbumsItem
+import retrofit2.Response
+import retrofit2.http.*
+
+interface AlbumsAPI {
+
+    @GET("/albums")
+    suspend fun getAlbums(): Response<Albums>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId: Int): Response<Albums>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") albumId: Int): Response<AlbumsItem>
+
+    @POST("/albums")
+    suspend fun uploadAlbum(@Body album: AlbumsItem): Response<AlbumsItem>
+}
